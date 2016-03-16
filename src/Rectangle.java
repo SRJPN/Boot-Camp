e96f4f37a6b6c87eab472154daaf0d53d9762986
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-03-16 14:29:51 +0530

    Done with simple rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,14 @@
+
+public class Rectangle {
+    double length;
+    double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea(){
+        return length*width;
+    }
+}
---
+++ test/RectangleTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RectangleTest {
+
+    @Test
+    public void test_getArea_gives_the_area_of_the_rectangle_created() throws Exception {
+        Rectangle rectangle = new Rectangle(12, 6);
+        double expected = 72;
+        Assert.assertTrue(expected == rectangle.getArea());
+    }
+
+    @Test
+    public void test_getArea_gives_the_area_of_the_rectangle_created_with_decimal_values_as_sides() throws Exception {
+        Rectangle rectangle = new Rectangle(7.5, 5);
+        double expected = 37.5;
+        Assert.assertTrue(expected == rectangle.getArea());
+    }
+}
